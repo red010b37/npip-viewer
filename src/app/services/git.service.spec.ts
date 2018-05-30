61ae8c703d847a6aaefa490f41ed88c532d830d2
red010b37 <paul@creativetech.io>
2018-05-30 21:12:37 +1200

    Get the correct url working by TDD

--- src/app/services/git.service.spec.ts
+++ src/app/services/git.service.spec.ts
@@ -1,10 +1,15 @@
 import { TestBed, inject } from '@angular/core/testing';
 
 import { GitService } from './git.service';
+import {HttpClientTestingModule} from "@angular/common/http/testing";
+import {environment} from "../../environments/environment";
 
 describe('GitService', () => {
   beforeEach(() => {
     TestBed.configureTestingModule({
+      imports: [
+        HttpClientTestingModule
+      ],
       providers: [GitService]
     });
   });
@@ -12,4 +17,69 @@ describe('GitService', () => {
   it('should be created', inject([GitService], (service: GitService) => {
     expect(service).toBeTruthy();
   }));
+
+
+  describe('buildNipUrl()', () => {
+
+    beforeEach(() => {
+      environment.NpipUrl = 'url/';
+    });
+
+    it('should build url correctly passing 1', inject([GitService], (service: GitService) => {
+      let expectUrl = `${environment.NpipUrl}0001.mediawiki`;
+      expect(service.buildNipUrl(1)).toEqual(expectUrl);
+    }));
+
+    it('should build url correctly passing 5', inject([GitService], (service: GitService) => {
+      let expectUrl = `${environment.NpipUrl}0005.mediawiki`;
+      expect(service.buildNipUrl(5)).toEqual(expectUrl);
+    }));
+
+    it('should build url correctly passing 9', inject([GitService], (service: GitService) => {
+      let expectUrl = `${environment.NpipUrl}0009.mediawiki`;
+      expect(service.buildNipUrl(9)).toEqual(expectUrl);
+    }));
+
+    it('should build url correctly passing 10', inject([GitService], (service: GitService) => {
+      let expectUrl = `${environment.NpipUrl}0010.mediawiki`;
+      expect(service.buildNipUrl(10)).toEqual(expectUrl);
+    }));
+
+    it('should build url correctly passing 57', inject([GitService], (service: GitService) => {
+      let expectUrl = `${environment.NpipUrl}0057.mediawiki`;
+      expect(service.buildNipUrl(57)).toEqual(expectUrl);
+    }));
+
+    it('should build url correctly passing 99', inject([GitService], (service: GitService) => {
+      let expectUrl = `${environment.NpipUrl}0099.mediawiki`;
+      expect(service.buildNipUrl(99)).toEqual(expectUrl);
+    }));
+
+    it('should build url correctly passing 100', inject([GitService], (service: GitService) => {
+      let expectUrl = `${environment.NpipUrl}0100.mediawiki`;
+      expect(service.buildNipUrl(100)).toEqual(expectUrl);
+    }));
+
+    it('should build url correctly passing 692', inject([GitService], (service: GitService) => {
+      let expectUrl = `${environment.NpipUrl}0692.mediawiki`;
+      expect(service.buildNipUrl(692)).toEqual(expectUrl);
+    }));
+
+    it('should build url correctly passing 999', inject([GitService], (service: GitService) => {
+      let expectUrl = `${environment.NpipUrl}0999.mediawiki`;
+      expect(service.buildNipUrl(999)).toEqual(expectUrl);
+    }));
+
+    it('should build url correctly passing 1000', inject([GitService], (service: GitService) => {
+      let expectUrl = `${environment.NpipUrl}1000.mediawiki`;
+      expect(service.buildNipUrl(1000)).toEqual(expectUrl);
+    }));
+
+    it('should build url correctly passing 8975', inject([GitService], (service: GitService) => {
+      let expectUrl = `${environment.NpipUrl}8975.mediawiki`;
+      expect(service.buildNipUrl(8975)).toEqual(expectUrl);
+    }));
+
+  });
+
 });
--- src/app/services/git.service.ts
+++ src/app/services/git.service.ts
@@ -1,9 +1,43 @@
 import { Injectable } from '@angular/core';
+import {HttpClient} from "@angular/common/http";
+import {environment} from "../../environments/environment";
 
 @Injectable({
   providedIn: 'root'
 })
 export class GitService {
 
-  constructor() { }
+  npipCounter: number = 1;
+
+  constructor(
+    private _http: HttpClient
+  ) {
+
+    this.getNpipData();
+  }
+
+
+  getNpipData() {
+
+
+
+    //this._http.get();
+
+  }
+
+  buildNipUrl(counter): string {
+
+    let strCounter = counter.toString();
+
+    let strZ = strCounter.split('');
+
+    for(let i =strZ.length ; i < 4 ; i++){
+      strZ.unshift('0')
+    }
+    let str = strZ.join('');
+
+    return `${environment.NpipUrl}${str}.mediawiki`;
+
+  }
+
 }
